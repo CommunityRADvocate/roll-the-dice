c4b0dda75fa3116ce6e39a547e160d81abcac622
Remy Drain <remydrain@gmail.com>
2023-11-03 13:01:01 -0400

    Add pseudocode to script

---
+++ script.js
@@ -0,0 +1,5 @@
+// get page
+// get dice
+// for each click on dice, add one die to roll
+// click on tray to roll selected dice
+    // if no dice have been selected, consider tray click a quick roll of a d20
